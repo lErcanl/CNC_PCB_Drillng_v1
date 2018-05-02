39a3a53013441b98cc45d81316b21f96a8a4e8f2
Ercan Can <38925279+lErcanl@users.noreply.github.com>
2018-05-03 00:16:13 +0300

    Add files via upload

---
+++ Interface class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yöntusları
+{
+    class Class1
+    {
+            
+        public string a { get; set; }
+        public string b { get; set; }
+        public string c { get; set; }
+        public string d { get; set; }
+        public string e{ get; set; }
+        public string f { get; set; }
+        public string g { get; set; }
+
+
+    }
+}
